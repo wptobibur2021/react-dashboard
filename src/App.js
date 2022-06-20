90cb38c62e51d1591e312334f06f0fc84411c2ca
Md Tobibur Rohman <wpdevelopment20@gmail.com>
2022-06-20 19:42:03 +0600

    'dummydata'

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { FiSettings } from 'react-icons/fi'
 import { TooltipComponent } from '@syncfusion/ej2-react-popups'
+import { Navbar, Sidebar } from './components'
 import './App.css'
 const App = () => {
     const activeMenu = true
@@ -19,17 +20,17 @@ const App = () => {
                     {
                         activeMenu ? (
                             <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
-                                Sidebar
+                                <Sidebar />
                             </div>
                         ) : (
                             <div className="w-0 dark:bg-secondary-dark-bg">
-                                sidebar w-0
+                                <Sidebar />
                             </div>
                         )
                     }
                     <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
                         <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
-                            <h1>Navbar</h1>
+                            <Navbar />
                         </div>
                     </div>
                     <div>
